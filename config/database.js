5b41e4f0d36f696f7a2f048da3cd12c5615fd234
Jhon Jairo Garcia <beakman@MacBook-Pro-de-Jhon.local>
2022-10-04 22:40:59 -0500

    se añadieron campos al user

--- config/database.js
+++ config/database.js
@@ -1,13 +1,12 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "touroloco_db"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "garcia12"),
     },
   },
 });
